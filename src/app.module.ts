1dbff8f8b7b2451650d6f23d5282f1f9ddfece2c
Caio Rebert <caiorebert070802.cr@gmail.com>
2025-08-18 10:01:59 -0300

    adicionando parâmetros de banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,13 +16,11 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'homefinance',
-      type: 'postgres',
-      synchronize: true,
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || '1234',
+      database: process.env.POSTGRES_DATABASE || 'homefinance',
       autoLoadEntities: true,
       logging: true,
       entities: [
